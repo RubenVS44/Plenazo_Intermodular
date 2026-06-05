57259d8946c057f0620e3ebcce08044af00dccd1
Pablo Ventero <pabloventero07@gmail.com>
2026-06-05 08:48:04 +0200

    Delete Codigo/Codigo_bolera directory

--- Codigo/Codigo_bolera/src/main/java/Excepciones/PistaOcupadaException.java
+++
@@ -1,7 +0,0 @@
-package Excepciones;
-
-public class PistaOcupadaException extends Exception {
-    public PistaOcupadaException(String mensaje) {
-        super(mensaje);
-    }
-}
--- Codigo/Codigo_bolera/src/main/java/Excepciones/RegistroJugadorException.java
+++
@@ -1,7 +0,0 @@
-package Excepciones;
-
-public class RegistroJugadorException extends Exception{
-    public RegistroJugadorException(String mensaje) {
-        super(mensaje);
-    }
-}
